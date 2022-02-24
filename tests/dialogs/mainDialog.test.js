b2ee408831cfb98bf4bc17219a65013c9b765c3d
Martin Burri <martin_burri95@gmx.ch>
2022-02-24 12:47:36 +0100

    fixt testing file

--- tests/dialogs/mainDialog.test.js
+++ tests/dialogs/mainDialog.test.js
@@ -21,27 +21,5 @@ describe('MainDialog', () =>{
         assert.strictEqual(reply.text, 'No Attachment, please upload an image.');
     })
 
-
-    it('Attachments', async()=>{
-        const mockMemoryStorage = new MemoryStorage();
-        const mockUserState = new UserState(mockMemoryStorage);
-        const testDialog = new MainDialog(mockUserState);
-        const client = new DialogTestClient('test', testDialog);
-        
-        function getInlineAttachment(){
-            const imageData = fs.readFileSync(path.join(__dirname, '../testdata/testimage.jpg'));
-            const base64Image = Buffer.from(imageData).toString('base64');
-            return {
-                name: 'testimage.jpg',
-                contentType: 'image/jpg',
-                contentUrl: `data:image/jpg;base64,${ base64Image }`
-            };
-        }
-        let text = { type: ActivityTypes.Message };
-        text.attachments = [getInlineAttachment()];
-        text.attachments.content.downlaodUrl = path.join(__dirname, '../testdata/testimage.jpg');
-        const reply = await client.sendActivity(text)
-        assert.strictEqual(reply.text, );
-    })
 })
 
